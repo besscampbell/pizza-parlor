f197c96199f92f9b2272b2fd2576cd80668918bf
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-06 11:50:37 -0800

    Add styling images to button and jumbotron.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
+// Business Logic------
 function Pizza (toppings, size, cost) {
   this.toppings = toppings;
   this. size = size;
   this.cost = cost;
 }
-// let pizza = new Pizza(["cheese", "pepperoni", "anchovies"], 10, 0)
 
 Pizza.prototype.calculateCost = function() {
    this.cost = (this.toppings.length * 2) + this.size
 }
 
-
+// User Interface Logic------
 $(document).ready(function(){
   $("#pizza-order").submit(function(event){
     event.preventDefault();
